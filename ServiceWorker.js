30c8b8bd88b83126f9603fa83a9a733ed926592c
TTOTT <kxdd2090@gmail.com>
2025-04-13 02:01:27 -0500

    v1
    v2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-KSEA-0.1";
 const contentToCache = [
     "Build/KSEA.loader.js",
-    "Build/KSEA.framework.js.unityweb",
-    "Build/KSEA.data.unityweb",
-    "Build/KSEA.wasm.unityweb",
+    "Build/KSEA.framework.js",
+    "Build/KSEA.data",
+    "Build/KSEA.wasm",
     "TemplateData/style.css"
 
 ];
